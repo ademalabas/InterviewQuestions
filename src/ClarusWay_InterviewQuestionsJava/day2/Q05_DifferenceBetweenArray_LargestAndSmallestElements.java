fb444193595f381c6f66ae9b86bacbc494ed3c1a
ademalabas@gmail.com <ademalabas@gmail.com>
2022-11-20 22:38:31 +0300

    20.11.22

--- src/ClarusWay_InterviewQuestionsJava/day2/Q05_DifferenceBetweenArray_LargestAndSmallestElements.java
+++ src/ClarusWay_InterviewQuestionsJava/day2/Q05_DifferenceBetweenArray_LargestAndSmallestElements.java
@@ -12,4 +12,21 @@ public class Q05_DifferenceBetweenArray_LargestAndSmallestElements {
     en kucuk elemanının  farkını bulan bir method create ediniz.
  */
 
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("array uzunluk :");
+        int uzunluk = scanner.nextInt();
+        int [] arr = new int[uzunluk];
+        for (int i = 0; i < uzunluk; i++) {
+            System.out.println("arrayin " + (i+1) + ".elemani :");
+            arr[i] = scanner.nextInt();
+        }
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+        System.out.println("differance :" +(arr[uzunluk-1] - arr[0]));
+    }
+
+
+
 }
--- src/InterviewQuestions/Pages08_25/Java_P012_Q008_CheckAlphabetIsVowelorConsonant.java
+++ src/InterviewQuestions/Pages08_25/Java_P012_Q008_CheckAlphabetIsVowelorConsonant.java
@@ -3,6 +3,26 @@ package InterviewQuestions.Pages08_25;
 
 public class Java_P012_Q008_CheckAlphabetIsVowelorConsonant {
     public static void main(String[] args) {
+//Question :Check Whether an Alphabet is Vowel or Consonant
+//a e i o u
+        char ch ='c';
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+            System.out.println(ch+ "  is vowel");
+        }else {
+            System.out.println(ch+ " is consonant");
+        }
+         // Second Way
+         switch (ch) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':System.out.println(ch+ " is vowel");break;
+             default:System.out.println(ch+ " is consonant");break;
+
+         }
+
+
 
 
 
