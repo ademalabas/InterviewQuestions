6520a41e685faf4ab3898717b6c6db2b88a60464
ademalabas@gmail.com <ademalabas@gmail.com>
2022-11-20 23:03:13 +0300

    20.11.22

--- src/InterviewQuestions/Pages08_25/Java_P013_Q009_LargestNumber.java
+++ src/InterviewQuestions/Pages08_25/Java_P013_Q009_LargestNumber.java
@@ -4,6 +4,34 @@ public class Java_P013_Q009_LargestNumber {
     public static void main(String[] args) {
 // Question : largest and smallest number findings
 
+         int x = 600;
+         int y = 400;
+         int z = 200;
+         if (x>y && x>z){
+             System.out.println("x is greatest");
+         }else if (y>z){
+             System.out.println("y is greatest");
+         }else {
+             System.out.println("z is greatest");
+         }
+
+         // 2nd way
+        if (x>y){
+            if (x>z) {
+                System.out.println("x is greatest");
+            }else {
+                System.out.println("z is greatest");
+            }
+        }else {
+            if (y>z) {
+                System.out.println("y is greatest");
+            }else {
+                System.out.println("z is greatest");
+            }
+        }
+
+
+
 
 
     }
