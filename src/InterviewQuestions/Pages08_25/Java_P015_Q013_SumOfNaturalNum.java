8a0b542b4e4a9c9b8e8fa2f108f9749d8620e789
ademalabas@gmail.com <ademalabas@gmail.com>
2022-11-20 23:53:25 +0300

    20.11.22

--- src/InterviewQuestions/Pages08_25/Java_P015_Q013_SumOfNaturalNum.java
+++ src/InterviewQuestions/Pages08_25/Java_P015_Q013_SumOfNaturalNum.java
@@ -2,7 +2,14 @@ package InterviewQuestions.Pages08_25;
 
 public class Java_P015_Q013_SumOfNaturalNum {
     public static void main(String[] args) {
-
+        double num =-25;
+        if (num <0.0){
+            System.out.println(num + " is negative");
+        }else if (num >0.0){
+            System.out.println(num + " is positive");
+        }else{
+            System.out.println( num + " is zero");
+        }
 
 
     }
