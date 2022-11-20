73d0467054bb77710b1aa390dab282ae47e5ef8b
ademalabas@gmail.com <ademalabas@gmail.com>
2022-11-20 23:46:38 +0300

    20.11.22

--- src/InterviewQuestions/Pages08_25/Java_P014_Q010_LeapYear.java
+++ src/InterviewQuestions/Pages08_25/Java_P014_Q010_LeapYear.java
@@ -2,7 +2,35 @@ package InterviewQuestions.Pages08_25;
 
 public class Java_P014_Q010_LeapYear {
     public static void main(String[] args) {
+        // Question : Find the leap year
+        int year = 2022;
+       boolean leapYear = false;
+        if (year %4 ==0){
+            if (year %100 ==0){
+                if (year %400 ==0){
+                    leapYear = true;
+                }else {
+                    leapYear = false;
+                }
+            }else {
+                leapYear = true;
+            }
+        }else {
+            leapYear = false;
+        }
+        if (leapYear){
+            System.out.println( year + " is a Leap Year");
+        }else {
+            System.out.println(year +" is not a Leap Year");
+        }
+// 2nd Way
+        System.out.println("*** 2nd way**");
 
+        if (year%4 ==0 && year % 100 !=0 || year % 400 ==0 ){
+            System.out.println( year + " is a Leap Year");
+        }else {
+            System.out.println(year +" is not a Leap Year");
+        }
 
 
     }
