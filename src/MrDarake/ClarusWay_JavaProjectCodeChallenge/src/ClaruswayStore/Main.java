2e7a4e0edfc0127663c151b1cd0c9a16be26a03d
ademalabas@gmail.com <ademalabas@gmail.com>
2022-12-10 22:13:18 +0300

    10.12.22

---
+++ src/MrDarake/ClarusWay_JavaProjectCodeChallenge/src/ClaruswayStore/Main.java
@@ -0,0 +1,9 @@
+package MrDarake.ClarusWay_JavaProjectCodeChallenge.src.ClaruswayStore;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Store claruswayStore = new Store();
+        claruswayStore.run();
+    }
+}
